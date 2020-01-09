d45f42fe42519cab20eb5ed7752820670219054d
khushabookhudia <45007350+khushabookhudia@users.noreply.github.com>
2020-01-09 18:32:29 +0530

    Add files via upload

---
+++ InstallCodeDeployAgent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo yum update -y 
+sudo yum install wget ruby -y 
+wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+
